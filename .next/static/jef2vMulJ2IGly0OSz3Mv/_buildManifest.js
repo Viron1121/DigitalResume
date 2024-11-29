a81f4e770527aa4e4c967ac11af5b40729b7fccc
Viron Navarro <VNavarro@ncmh.gov.ph>
2024-11-29 11:18:47 +0800

    resolve eslint off rules

---
+++ .next/static/jef2vMulJ2IGly0OSz3Mv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-a412d67dd2489312.js","static/chunks/pages/index-a90dc7ccf8cf2b24.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/components/Sections/Facemesh.tsx
+++ src/components/Sections/Facemesh.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Facemesh = () => {
-  // component logic here
   return <div>Facemesh Component</div>;
 };
 
-// Wrap the component with React.memo
 export default React.memo(Facemesh);
