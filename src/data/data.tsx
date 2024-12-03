727d6c72a8d9770b3518afe5f93fbc3ba42a735d
Viron Navarro <VNavarro@ncmh.gov.ph>
2024-12-03 15:50:15 +0800

    low light on logo

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -87,7 +87,7 @@ export const heroData: Hero = {
           // src="/assets/react.gif" //for local
           src="assets/react.gif" //for main branch deployed
           style={{
-            filter: 'drop-shadow(0 0 5px rgba(255, 255, 255, 0.7)) drop-shadow(0 0 10px rgba(255, 255, 255, 0.6)) drop-shadow(0 0 15px rgba(255, 255, 255, 0.5))',
+            filter: 'drop-shadow(0 0 5px rgba(100, 100, 255, 0.7)) drop-shadow(0 0 10px rgba(255, 255, 255, 0.6)) drop-shadow(0 0 15px rgba(100, 100, 255, 0.5))',
             margin: 0, 
             padding: 0,
           }} 
