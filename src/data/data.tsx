7e9172396b807b2774367f2b9470ead912647ace
Viron Navarro <VNavarro@ncmh.gov.ph>
2024-12-03 14:02:24 +0800

    space on first word

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -74,7 +74,7 @@ export const heroData: Hero = {
   description: (
     <>
      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg items-center">
-      A 
+      A {''}
       <strong style={{color: 'white', textShadow: '0 0 5px rgba(255, 255, 255, 0.7), 0 0 10px rgba(255, 255, 255, 0.6), 0 0 15px rgba(255, 255, 255, 0.5)'}}>
         Full Stack Web Developer
       </strong>, currently working at{' '}
